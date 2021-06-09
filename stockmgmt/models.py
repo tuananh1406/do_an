fd2fd80c649b5e128d702b4e9177bbb5ecf03f4f
htuananh <anhnguyenhuutuan@vccorp.vn>
2021-06-09 09:23:30 +0700

    Cập nhật trường exp

--- stockmgmt/models.py
+++ stockmgmt/models.py
@@ -30,6 +30,7 @@ class Stock(models.Model):
     reorder_level = models.IntegerField(default='0', blank=True, null=True)
     last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    exp_day = models.DateTimeField()
 
     def __str__(self):
         return self.item_name + ' ' + str(self.quantity)
